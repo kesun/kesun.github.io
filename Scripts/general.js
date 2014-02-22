f5d41d85de04eaa34a51c174341ac3479419e030
Ke Sun <kesun15@gmail.com>
2014-02-21 23:39:51 -0500

    Using jQueryRotate for a smooth rotating animation of the tiny wings

--- Scripts/general.js
+++ Scripts/general.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
        });
 	   jQuery('.button', this).fadeIn('slow');
 	   jQuery('.buttonText', this).fadeIn('fast');
-	   $('#left-wing').css('transform', 'rotate(5deg)').css('-ms-transform', 'rotate(5deg)').css('-webkit-transform', 'rotate(5deg)');
-	   $('#right-wing').css('transform', 'rotate(-5deg)').css('-ms-transform', 'rotate(-5deg)').css('-webkit-transform', 'rotate(-5deg)');
+	   $('#left-wing').rotate({animateTo:5});
+	   $('#right-wing').rotate({animateTo:-5});
    }).mouseleave(function() {
        $(this).animate({
            height: '-=370px'
        }); 
 	   jQuery('.button', this).hide();
 	   jQuery('.buttonText', this).hide();
-	   $('#left-wing').css('transform', 'rotate(-5deg)').css('-ms-transform', 'rotate(-5deg)').css('-webkit-transform', 'rotate(-5deg)');
-	   $('#right-wing').css('transform', 'rotate(5deg)').css('-ms-transform', 'rotate(5deg)').css('-webkit-transform', 'rotate(5deg)');
+	   $('#left-wing').rotate({animateTo:-5});
+	   $('#right-wing').rotate({animateTo:5});
    });
 });
