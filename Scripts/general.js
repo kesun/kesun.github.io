d0b9c37568b8fc77ca07c114f11932f7560650c9
Ke Sun <kesun15@gmail.com>
2014-02-21 23:49:20 -0500

    Speed up the rotational speed to 0.2s, change in angle is 10 deg

--- Scripts/general.js
+++ Scripts/general.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
        });
 	   jQuery('.button', this).fadeIn('slow');
 	   jQuery('.buttonText', this).fadeIn('fast');
-	   $('#left-wing').rotate({animateTo:5});
-	   $('#right-wing').rotate({animateTo:-5});
+	   $('#left-wing').rotate({animateTo:10, duration: 200});
+	   $('#right-wing').rotate({animateTo:-10, duration: 200});
    }).mouseleave(function() {
        $(this).animate({
            height: '-=370px'
        }); 
 	   jQuery('.button', this).hide();
 	   jQuery('.buttonText', this).hide();
-	   $('#left-wing').rotate({animateTo:-5});
-	   $('#right-wing').rotate({animateTo:5});
+	   $('#left-wing').rotate({animateTo:-10});
+	   $('#right-wing').rotate({animateTo:10});
    });
 });
