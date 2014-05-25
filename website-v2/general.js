9f51602f5f1923ff895647ccdf85d79d3cbffa76
Ke Sun <kesun15@gmail.com>
2014-05-25 01:59:55 -0400

    minor changes

--- website-v2/general.js
+++ website-v2/general.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	animate($('#circle-outer'), 2000, 0, 0.4, 0.0, 80, 130, "#510022", "#FFFFFF");
-	animate($('#circle-inner'), 1000, 1000, 1, 0.5, 90, 120, "#510022", "#510022");
+	animate($('#circle-inner'), 1000, 1000, 1, 0.5, 90, 125, "#510022", "#510022");
 });
 
 function animate(id, speed, del, initOpa, finalOpa, initDia, finalDia, initColor, finalColor){
@@ -13,13 +13,13 @@ function animate(id, speed, del, initOpa, finalOpa, initDia, finalDia, initColor
 		"background-color": finalColor,
 		opacity: finalOpa
 	}, speed, "easeInOutCubic", function(){
-		id.css('width', initDia + "px")
+		id.css('background-color', initColor)
+			.css('width', initDia + "px")
 			.css('height', initDia + "px")
 			.css('opacity', initOpa)
 			.css('border-radius', initDia / 2 + "px")
 			.css('margin-top', -1 * initDia / 2 + "px")
-			.css('margin-left', -1 * initDia / 2 + "px")
-			.css('background-color', initColor);
+			.css('margin-left', -1 * initDia / 2 + "px");
 		animate(id, speed, del, initOpa, finalOpa, initDia, finalDia, initColor, finalColor);
 	});
 }
