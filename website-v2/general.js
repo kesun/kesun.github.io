abdb77f7183e8c86a4394bcba765824159f7a116
Ke Sun <kesun15@gmail.com>
2014-05-26 00:55:35 -0400

    fixed

--- website-v2/general.js
+++ website-v2/general.js
@@ -1,25 +1,23 @@
 $(document).ready(function(){
-	animate($('#circle-outer'), 2000, 0, 0.4, 0.0, 80, 130, "#510022", "#FFFFFF");
-	animate($('#circle-inner'), 1000, 1000, 1, 0.5, 90, 125, "#510022", "#510022");
+	animate($('#circle-outer'), 2000, 0, 84, 120, "#FFFFFF", "#510022");
+	animate($('#circle-inner'), 1000, 1000, 100, 125, "#510022", "#510022");
 });
 
-function animate(id, speed, del, initOpa, finalOpa, initDia, finalDia, initColor, finalColor){
+function animate(id, speed, del, initDia, finalDia, initColor, finalColor){
 	id.delay(del).animate({
 		width: finalDia + "px",
 		height: finalDia + "px",
 		"margin-top": -1 * finalDia / 2 + "px",
 		"margin-left": -1 * finalDia / 2 + "px",
 		"border-radius": finalDia / 2 + "px",
-		"background-color": finalColor,
-		opacity: finalOpa
+		"background-color": finalColor
 	}, speed, "easeInOutCubic", function(){
 		id.css('background-color', initColor)
 			.css('width', initDia + "px")
 			.css('height', initDia + "px")
-			.css('opacity', initOpa)
 			.css('border-radius', initDia / 2 + "px")
 			.css('margin-top', -1 * initDia / 2 + "px")
 			.css('margin-left', -1 * initDia / 2 + "px");
-		animate(id, speed, del, initOpa, finalOpa, initDia, finalDia, initColor, finalColor);
+		animate(id, speed, del, initDia, finalDia, initColor, finalColor);
 	});
 }
